7f7796a51ade0c98082ce168891dc86260cb224a
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-03-31 11:17:44 +0200

    Remove stray browser() call

--- R/get_pkgs_metadata.R
+++ R/get_pkgs_metadata.R
@@ -11,7 +11,6 @@ get_pkgs_metadata <- function(universe = "epiverse-connect") {
     purrr::map(\(x) {
       x$articles <- unlist(purrr::map(x$`_vignettes`, "filename"))
       x$articles <- glue::glue("https://{universe}.r-universe.dev/articles/{x$Package}/{x$articles}")
-      browser()
       x$URL_list <- stringr::str_split(x$URL, "[,\\n[:space:]]+")
       # First URL that doesn't look like a link to a GitHub repo
       x$docs_URL <- purrr::map_chr(x$URL_list, ~ .x[match(FALSE, startsWith(.x, "https://github.com"))])
